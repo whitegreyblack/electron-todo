19eb4b353af0a5978509648635e33b460282ff67
Sam Whang <sangwoowhang@gmail.com>
2018-04-24 15:44:27 -0500

    Added js functionality to buttons and title-bar

--- todo/js/frameless_window.js
+++ todo/js/frameless_window.js
@@ -1,37 +1,37 @@
-function updateCheckbox() {
-  var top_checkbox = document.getElementById("top-box");
-  var bottom_checkbox = document.getElementById("bottom-box");
-  var left_checkbox = document.getElementById("left-box");
-  var right_checkbox = document.getElementById("right-box");
-  if (top_checkbox.checked || bottom_checkbox.checked) {
-    left_checkbox.disabled = true;
-    right_checkbox.disabled = true;
-  } else if (left_checkbox.checked || right_checkbox.checked) {
-    top_checkbox.disabled = true;
-    bottom_checkbox.disabled = true;
-  } else {
-    left_checkbox.disabled = false;
-    right_checkbox.disabled = false;
-    top_checkbox.disabled = false;
-    bottom_checkbox.disabled = false;
-  }
-}
+// function updateCheckbox() {
+//   var top_checkbox = document.getElementById("top-box");
+//   var bottom_checkbox = document.getElementById("bottom-box");
+//   var left_checkbox = document.getElementById("left-box");
+//   var right_checkbox = document.getElementById("right-box");
+//   if (top_checkbox.checked || bottom_checkbox.checked) {
+//     left_checkbox.disabled = true;
+//     right_checkbox.disabled = true;
+//   } else if (left_checkbox.checked || right_checkbox.checked) {
+//     top_checkbox.disabled = true;
+//     bottom_checkbox.disabled = true;
+//   } else {
+//     left_checkbox.disabled = false;
+//     right_checkbox.disabled = false;
+//     top_checkbox.disabled = false;
+//     bottom_checkbox.disabled = false;
+//   }
+// }
 
-function initCheckbox(checkboxId, titlebar_name, titlebar_icon_url, titlebar_text) {
-  var elem = document.getElementById(checkboxId);
-  if (!elem)
-    return;
-  elem.onclick = function() {
-    if (document.getElementById(checkboxId).checked)
-      addTitlebar(titlebar_name, titlebar_icon_url, titlebar_text);
-    else
-      removeTitlebar(titlebar_name);
-    focusTitlebars(true);
+// function initCheckbox(checkboxId, titlebar_name, titlebar_icon_url, titlebar_text) {
+//   var elem = document.getElementById(checkboxId);
+//   if (!elem)
+//     return;
+//   elem.onclick = function() {
+//     if (document.getElementById(checkboxId).checked)
+//       addTitlebar(titlebar_name, titlebar_icon_url, titlebar_text);
+//     else
+//       removeTitlebar(titlebar_name);
+//     focusTitlebars(true);
 
-    updateContentStyle();
-    updateCheckbox();
-  }
-}
+//     updateContentStyle();
+//     updateCheckbox();
+//   }
+// }
 
 function initTitleBar(checkboxId, titlebar_name, titlebar_icon_url, titlebar_text) {
   var elem = document.getElementById(checkboxId);
@@ -53,57 +53,57 @@ window.onblur = function() {
   // focusTitlebars(false);
 }
 
-window.onresize = function() {
-  updateContentStyle();
+window.onresize = function(event) {
+  console.log("resize");
+  event.preventDefault();
+  window.resizeTo(375, 675);
+  // updateContentStyle();
 }
 
 window.onload = function() {
-  // initTitleBar("top-box", "top-titlebar", "static/tasks.png", "Top Titlebar");
-  // initCheckbox("top-box", "top-titlebar", "top-titlebar.png", "Top Titlebar");
-  // initCheckbox("bottom-box", "bottom-titlebar", "bottom-titlebar.png", "Bottom Titlebar");
-  // initCheckbox("left-box", "left-titlebar", "left-titlebar.png", "Left Titlebar");
-  // initCheckbox("right-box", "right-titlebar", "right-titlebar.png", "Right Titlebar");
   const {remote} = require('electron');
   const {BrowserWindow} = remote;
   const win = BrowserWindow.getFocusedWindow();
-  document.getElementById("content").onclick = function() {
-    console.log("CLICKED");
-  }
-  // document.getElementById("close-window-button").onclick = function() {
-  //   win.close();
-  // }
-  // document.getElementById("minimize-window-button").onclick = function() {
-  //   win.minimize();
-  // }
-  // document.getElementById("maximize-window-button").onclick = function() {
-  //   win.maximize();
-  // }
-  // document.getElementById("unmaximize-window-button").onclick = function() {
-  //   win.unmaximize();
-  // }
+
   // document.getElementById("toggle-window-button").onclick = function() {
   //   win.setFullScreen(!(win.isFullScreen()));
   // }
   // document.getElementById("maxmin-window-button").onclick = function() {
   //   win.isMaximized() ? win.unmaximize() : win.maximize();
   // }
-
-  // document.getElementById("min").onclick = function() {
-  //   win.minimize();
-  // }
   // document.getElementById("max").onclick = function() {
   //   win.isMaximized() ? win.unmaximize() : win.maximize();
   // }
-  // document.getElementById("exit").onclick = function() {
-  //   win.close();
-  // }
+
   document.getElementById("btn-minify").onclick = function() {
     win.minimize();
   }
-
+  document.getElementById("btn-maxify").onclick = function() {
+    console.log("maximized?: " + win.isMaximized() ? "true" : "false");
+  }
   document.getElementById("btn-close").onclick = function() {
     win.close();
   }
 
+  // exit app keypress
+  document.onkeydown = function(event) {
+    var exitKey = false;
+    event = event || window.event;
+    
+    if ("key" in event) {
+      exitKey = (event.key == "Escape" || event.key == "Esc");
+    } else {
+      exitKey = (event.key == 27);
+    }
+
+    if (exitKey) {
+      document.getElementById("btn-close").click();
+    }
+  }
+  document.getElementById("title-bar").addEventListener("dblclick", function(event) {
+    console.log("double click");
+    event.preventDefault();
+  }, false);
   updateContentStyle();
+  
 }
